2b17503f0274031ee06840d0fcc09392cc70dae7
andrija-tosic <andonnet@gmail.com>
2024-01-12 16:00:58 +0100

    draft: selection transformations

--- src/main/kotlin/net/kvibews/handler/event/WebSocketEvent.kt
+++ src/main/kotlin/net/kvibews/handler/event/WebSocketEvent.kt
@@ -1,4 +1,3 @@
 package net.kvibews.handler.event
 
-data class Selection(val docId: String, val from: Int, val to: Int, val performedBy: String)
 data class WebSocketEvent<T>(val roomId: String, val payload: T)
